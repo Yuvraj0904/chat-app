8b7c332aab28bd46771b43b0f547c6b1d29c7dce
Yuvraj0904 <yuvrajsingh091124@gmail.com>
2026-06-30 21:21:27 +0530

    final commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
-
+import bgImg from "../assets/bg_img.png";
 const Home = () => {
   return (
     <div
+      style={{ backgroundImage: `url(${bgImg})` }}
       className='flex flex-col items-center justify-center min-h-screen
-      bg-[url("/bg_img.png")] bg-cover bg-center bg-cyan-100'
+      bg-cover bg-center bg-cyan-100'
     >
       <Navbar />
       <Header />
